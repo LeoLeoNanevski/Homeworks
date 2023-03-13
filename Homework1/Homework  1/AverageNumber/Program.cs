808dc637170ad5e41bc32661db9a92027b637ce6
LeoLeoNanevski <leonanevski87@gmail.com>
2023-03-13 22:21:38 +0100

    Add project files.

---
+++ Homework1/Homework  1/AverageNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please enter the First number:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the Second number:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the Third number:");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the Fourth number:");
+double num4 = Convert.ToDouble(Console.ReadLine());
+
+double avg = (num1 + num2 + num3 + num4) / 4;
+
+Console.WriteLine("The average of 4 entered numbers: " + num1 + ", " + num2 + ", " + num3 + " and " + num4 + " is: " + avg);
---
+++ Homework1/Homework  1/RealCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("This is Real Calculator with 2 numbers. Please enter the First number:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the Second number:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the desired operation (+, -, *, /):");
+string operation = Console.ReadLine();
+
+double result = 0;
+switch (operation)
+{
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    case "*":
+        result = num1 * num2;
+        break;
+    case "/":
+        result = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Error!!!");
+        break;
+}
+
+Console.WriteLine("The result of the operation is: " + result);
